ce4d812cb1fd30e05f0c5490cec339c9f2610084
Ohad Mizrachi <ohad24@gmail.com>
2021-07-21 16:02:21 +0300

    test_get_products_in_store

--- tests/api/test_stores.py
+++ tests/api/test_stores.py
@@ -1,11 +1,37 @@
 from conftest import client
 import json
 import pytest
+from db import get_db
+from models.store import Store
+from models.product import Product
 
 
 @pytest.fixture(scope="module")
-def data():
-    pytest.store_id = None
+def store_data():
+    db = get_db()
+    ret = db.stores.insert_one(
+        Store(**{"name": "store1", "description": "store desc 1"}).dict(by_alias=True)
+    )
+    store_date = db.stores.find_one({"_id": ret.inserted_id})
+    store = Store(**store_date)
+    return store
+
+
+@pytest.fixture(scope="module")
+def product_data(store_data):
+    db = get_db()
+    ret = db.products.insert_one(
+        Product(
+            **{
+                "name": "product1",
+                "description": "product desc 1",
+                "store_id": store_data.store_id,
+            }
+        ).dict(by_alias=True)
+    )
+    product_date = db.products.find_one({"_id": ret.inserted_id})
+    product = Product(**product_date)
+    return product
 
 
 def test_get_stores():
@@ -13,24 +39,32 @@ def test_get_stores():
     assert response.status_code == 200
 
 
-def test_create_store(data):
+def test_create_store():
     response = client.post(
         "/api/v1/stores/",
-        data=json.dumps({"name": "store1", "description": "store desc 1"})
+        data=json.dumps({"name": "store1", "description": "store desc 1"}),
     )
     assert response.status_code == 200
     assert "name" in response.json().get("store").keys()
     assert "store1" in response.json().get("store").get("name")
-    pytest.store_id = response.json().get("store").get("store_id")
+    # pytest.store_id = response.json().get("store").get("store_id")
 
 
-def test_get_store(data):
-    if not pytest.store_id:
-        test_create_store(data)
-    response = client.get(f"/api/v1/stores/{pytest.store_id}")
+def test_get_store(store_data):
+    response = client.get(f"/api/v1/stores/{store_data.store_id}")
     print(response.json())
     print(response.status_code)
     assert response.status_code == 200
 
-    response = client.get(f"/api/v1/stores/{pytest.store_id}" + "a")
+    response = client.get(f"/api/v1/stores/{store_data.store_id}" + "a")
+    assert response.status_code == 404
+
+
+def test_get_products_in_store(product_data):
+    response = client.get(f"/api/v1/stores/{product_data.store_id}/products")
+    assert response.status_code == 200
+    assert "products" in response.json().get("store").keys()
+    assert "product1" in response.json().get("store").get("products")[0].get("name")
+
+    response = client.get(f"/api/v1/stores/{product_data.store_id}a/products")
     assert response.status_code == 404
